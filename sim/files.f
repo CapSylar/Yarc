f0d1f13d1b1529fb53cdaaac3fa5cdf0fb652c97
robin <rkaraa@ubilite.com>
2023-09-18 11:36:04 +0300

    fixes questa compilation

--- sim/files.f
+++ sim/files.f
@@ -1,11 +1,19 @@
-# include directories
+// include directories
++incdir+${PRJ_DIR}/core/
 +incdir+${PRJ_DIR}/core/includes/
 
-# core
-${PRJ_DIR}/core/*
+//  core
+${PRJ_DIR}/core/core_top.sv
+${PRJ_DIR}/core/decode.sv
+${PRJ_DIR}/core/dep_hzrd_detection.sv
+${PRJ_DIR}/core/execute.sv
+${PRJ_DIR}/core/mem_rw.sv
+${PRJ_DIR}/core/reg_file.sv
+${PRJ_DIR}/core/simple_fetch.sv
+${PRJ_DIR}/core/write_back.sv
 
-# memories
+// memories
 ${PRJ_DIR}/memories/*
 
-# testbenches
+// testbenches
 ${PRJ_DIR}/testbenches/*
